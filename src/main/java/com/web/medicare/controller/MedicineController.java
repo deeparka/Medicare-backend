f1ac00666426eabcce3f933722929af71c76fb3d
deeparka <arkadeep180@gmail.com>
2023-03-29 10:46:48 +0530

    second commit

--- src/main/java/com/web/medicare/controller/MedicineController.java
+++ src/main/java/com/web/medicare/controller/MedicineController.java
@@ -1,8 +1,40 @@
 package com.web.medicare.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.web.medicare.dao.MedicineRepo;
+import com.web.medicare.model.Medicine;
 
 @RestController
 public class MedicineController {
-
+	
+	@Autowired
+	MedicineRepo medicineRepo;
+	
+	@PostMapping(path="/medicine/add")
+	public Medicine addMedicine(@RequestBody Medicine medicine) {
+		medicineRepo.save(medicine);
+		return medicine;
+	}
+	
+	@GetMapping(path="/medicines")
+	@ResponseBody
+	public List<Medicine> getMedicine() {
+		return medicineRepo.findAll();
+	}
+	
+	@PutMapping(path="/medicine/update")
+	public Medicine updateMedicine(@RequestBody Medicine medicine) {
+		medicineRepo.save(medicine);
+		return medicine;
+	}
+	
 }
--- src/main/java/com/web/medicare/controller/UserController.java
+++ src/main/java/com/web/medicare/controller/UserController.java
@@ -1,8 +1,39 @@
 package com.web.medicare.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.web.medicare.dao.UserRepo;
+import com.web.medicare.model.User;
 
 @RestController
 public class UserController {
-
+	
+	@Autowired
+	UserRepo userRepo;
+	
+	@PostMapping(path="/medicine/add")
+	public User addMedicine(@RequestBody User user) {
+		userRepo.save(user);
+		return user;
+	}
+	
+	@GetMapping(path="/medicines")
+	@ResponseBody
+	public List<User> getMedicine() {
+		return userRepo.findAll();
+	}
+	
+	@PutMapping(path="/medicine/update")
+	public User updateMedicine(@RequestBody User user) {
+		userRepo.save(user);
+		return user;
+	}
 }
